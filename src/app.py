0db77a61953cd4ba807dd22508ff67b6343de0d9
josedrosales <124944389+josedrosales@users.noreply.github.com>
2024-01-14 16:24:56 +0000

    changes

--- src/app.py
+++ src/app.py
@@ -1,12 +1,15 @@
-import os
+
 from sqlalchemy import create_engine
-import pandas as pd
 from dotenv import load_dotenv
+import os
 
-# load the .env file variables
 load_dotenv()
-
 # 1) Connect to the database here using the SQLAlchemy's create_engine function
+db_host = os.getenv("localhost")
+db_user = os.getenv("josedrosales")
+db_password = os.getenv("DB_PASSWORD")
+db_name = os.getenv("DB_NAME")
+
 
 # 2) Execute the SQL sentences to create your tables using the SQLAlchemy's execute function
 
